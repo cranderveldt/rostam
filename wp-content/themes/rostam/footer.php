df796eb15a41fb986fdaa00c1ea968c22464b1e4
cranderveldt <djnaphouse@gmail.com>
2014-05-29 14:53:38 -0400

    footer legal word changed

--- wp-content/themes/rostam/footer.php
+++ wp-content/themes/rostam/footer.php
@@ -3,7 +3,7 @@
 <footer id="footer" role="contentinfo">
 <div id="copyright">
 <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), 'Rostam Law' ); ?>
-<a href="/legal">Legal</a>
+<a href="/legal">Legal Notice</a>
 </div>
 </footer>
 </div>
