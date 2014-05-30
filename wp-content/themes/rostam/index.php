ece5773c7d4baa2a896af102d17fad29050726ce
cranderveldt <djnaphouse@gmail.com>
2014-05-30 08:55:55 -0400

    finished up the last people images, added blurb on blog, removed old styles for tiles, added images to interior engagement pages

--- wp-content/themes/rostam/index.php
+++ wp-content/themes/rostam/index.php
@@ -1,8 +1,9 @@
 <?php get_header(); ?>
 <section id="content" role="main">
 <div class="banner title-banner blog">
-<h1 class="h1">Blog</h1>
+<h1 class="h1">Rostam Wall</h1>
 </div>
+<p class="preamble">Welcome to the Rostam Wall. This is a place for thought, discussion, and debate. The purpose of the Wall is to educate, as well as leave our readers in a state of inquiry.</p>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php comments_template(); ?>
